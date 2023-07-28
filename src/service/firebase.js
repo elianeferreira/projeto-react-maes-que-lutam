e4a448a206e7df90689f30455f3378b94ddb2c27
elianeferreira <sanelisantos@hotmail.com>
2023-07-28 17:12:37 -0300

    inclusao key firebase

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -8,14 +8,14 @@ import {getDatabase} from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBA96Wjl0iXQrmUb1g8CiESycND6QU2Yx8",
-  authDomain: "projeto-maes-que-lutam.firebaseapp.com",
-  projectId: "projeto-maes-que-lutam",
-  storageBucket: "projeto-maes-que-lutam.appspot.com",
-  messagingSenderId: "817800894965",
-  appId: "1:817800894965:web:d5a9d3e046a98ce9a0a8bc",
-  measurementId: "G-6RFSC3L4D1",
-  databaseURL: "https://projeto-maes-que-lutam-default-rtdb.firebaseio.com/",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  measurementId: import.meta.env.VITE_APP_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
 };
 
 // Initialize Firebase
