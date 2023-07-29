4ff249d7c8729283154f6d36a2d9d14531b695c3
elianeferreira <sanelisantos@hotmail.com>
2023-07-28 23:13:43 -0300

    alteração firebase

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   measurementId: import.meta.env.VITE_APP_ID,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  databaseURL: import.meta.env.VITE_DATABASE_URL
 };
 
 // Initialize Firebase
